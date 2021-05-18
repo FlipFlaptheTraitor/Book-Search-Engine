d0b48f9977f1a75836bf923cf5cb8d9e4a46b266
FlipFlaptheTraitor <flipflapthetraitor@protonmail.com>
2021-05-17 23:16:42 -0400

    rdy for deployment and readme

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 import { useMutation } from '@apollo/react-hooks';
 import Auth from '../utils/auth';
-
+import { ADD_USER } from '../utils/mutations';
 const SignupForm = () => {
   // set initial form state
   const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
